48221f5babb3380e8091b5917a3ff24d3b614e67
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2024-06-03 17:56:51 +0200

    Implemented basic app layout

--- BMICalculatorApp/MainActivity.java
+++ BMICalculatorApp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.hayleydodkins.bmicalculator;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +21,7 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Toast.makeText(this,"Toasting the toast....",Toast.LENGTH_LONG).show();
     }
 }
