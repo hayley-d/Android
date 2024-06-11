1af1079fa211e42d12117b44812846080520bebd
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2024-06-11 17:10:32 +0200

    Initial Commit
    Practicing Kotlin variables

---
+++ ShamelessSelfPromoApp/scratch.kts
@@ -0,0 +1,14 @@
+var name : String = "Hayley"
+println(name)
+
+var age : Int = 21
+println(age)
+
+var female : Boolean = true
+println(female)
+
+//A constant is declared using val instead of ver
+val myConstant : Double = 77.7
+println(myConstant)
+
+val variable = "Bun" //inferred that it is a String do not need to declare it
