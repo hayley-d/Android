5be446de8a5ae47cd9ac657e83a4a168cb5d41b8
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2024-06-12 15:22:29 +0200

    Added Dependency

--- ShamelessSelfPromoApp/build.gradle.kts
+++ ShamelessSelfPromoApp/build.gradle.kts
@@ -33,7 +33,7 @@ android {
 }
 
 dependencies {
-
+    implementation (libs.material.v121)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
