f760b45f77272d78c9a489e15974cb656a362234
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2024-06-12 15:46:01 +0200

    Add Constants

--- ShamelessSelfPromoApp/MainActivity.kt
+++ ShamelessSelfPromoApp/MainActivity.kt
@@ -1,20 +1,57 @@
 package com.hayleydodkins.shamlessselfpromoapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.Spinner
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+
+    private var contactNameInput : TextInputEditText? = null
+    private var numberInput : TextInputEditText? = null
+    private var displayNameInput : TextInputEditText? = null
+
+    private var startDateInput : TextInputEditText? = null
+    private var juniorCheckBox : CheckBox? = null
+    private var immStartCheckBox : CheckBox? = null
+    private var jobTitleInput : Spinner? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val button : Button = findViewById(R.id.submit_button)
+
+        button.setOnClickListener{
+            Toast.makeText(this,"Creating Promo ...",Toast.LENGTH_LONG).show()
+            onSubmitClick()
         }
+    }
+
+
+    private fun onSubmitClick (){
+        //Get the input data
+         contactNameInput  = findViewById(R.id.edit_text_contact_name)
+         numberInput  = findViewById(R.id.edit_text_contact_number)
+         displayNameInput  = findViewById(R.id.edit_text_display_name)
+        startDateInput  = findViewById(R.id.edit_text_start)
+        juniorCheckBox  = findViewById(R.id.check_box_junior)
+        immStartCheckBox  = findViewById(R.id.check_box_im_start)
+        jobTitleInput  = findViewById(R.id.spinner_job_title)
+
+        //Convert into strings
+        val contactName = contactNameInput?.getText().toString()
+        val number = numberInput?.getText().toString()
+        val displayName = displayNameInput?.getText().toString()
+
+
     }
 }
