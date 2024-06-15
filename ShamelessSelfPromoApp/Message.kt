7b42b4e59d90a9397a342492b62b35d9b250e5fb
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2024-06-15 03:33:52 +0200

    Convert to serializable

--- ShamelessSelfPromoApp/Message.kt
+++ ShamelessSelfPromoApp/Message.kt
@@ -1,7 +1,11 @@
 package com.hayleydodkins.shamlessselfpromoapp
 
+import java.io.Serializable
+
 class Message(val contactName : String, val contactNumber : String, val displayName : String, val startDate : String, val junior : Boolean, val immidiateStart : Boolean, val jobTitle : String )
-{
+    : Serializable {
+
+    //Objects can be persisted through serialization
     
 
 }
