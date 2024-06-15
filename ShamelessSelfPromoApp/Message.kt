631d108f640b019c5e0c95d2badbacfef037712e
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2024-06-15 03:20:35 +0200

    Initial Commit

---
+++ ShamelessSelfPromoApp/Message.kt
@@ -0,0 +1,7 @@
+package com.hayleydodkins.shamlessselfpromoapp
+
+class Message(val contactName : String, val contactNumber : String, val displayName : String, val startDate : String, val junior : Boolean, val immidiateStart : Boolean, val jobTitle : String )
+{
+    
+
+}
