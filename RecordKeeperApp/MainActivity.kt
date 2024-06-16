35879ac965c430b30d603ccd6b7c898c8226c7eb
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2024-06-16 11:15:08 +0200

    Implement View binding

--- RecordKeeperApp/MainActivity.kt
+++ RecordKeeperApp/MainActivity.kt
@@ -1,16 +1,24 @@
-package com.hayleydodkins.recordkeeperapp
+package com.example.recordkeeperapp
 
 import android.os.Bundle
+import android.widget.FrameLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.recordkeeperapp.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
         
     }
 }
