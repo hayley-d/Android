95aab184787f9f397f7e793312032252747622d5
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2024-06-16 12:20:48 +0200

    Implement fragment switching

--- RecordKeeperApp/MainActivity.kt
+++ RecordKeeperApp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.recordkeeperapp
 
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.FrameLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -10,7 +11,7 @@ import androidx.fragment.app.commit
 import com.example.recordkeeperapp.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
     private lateinit var binding : ActivityMainBinding
     private lateinit var container : FrameLayout
     private lateinit var bottomNav : BottomNavigationView
@@ -25,10 +26,30 @@ class MainActivity : AppCompatActivity() {
         this.container = binding.frameContent
         this.bottomNav = binding.bottomNav
 
+        bottomNav.setOnNavigationItemSelectedListener (this)
+    }
+
+    override fun onNavigationItemSelected(item : MenuItem) : Boolean{
+        if(item.itemId.equals(R.id.nav_bike))
+        {
+            onCyclingClicked()
+            return true
+        }
+        else if(item.itemId.equals(R.id.nav_running))
+        {
+            onRunningClicked()
+            return true
+        }
+        return false
+    }
+
+    private fun onRunningClicked(){
         supportFragmentManager.commit {
             replace(R.id.frame_content, RunningFragment())
         }
+    }
 
+    private fun onCyclingClicked(){
         supportFragmentManager.commit {
             replace(R.id.frame_content, CyclingFragment())
         }
