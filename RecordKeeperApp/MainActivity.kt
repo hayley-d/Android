2ebac296b43797e361c95650d261ff9adc09d215
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2024-06-16 11:40:38 +0200

    Add fragment

--- RecordKeeperApp/MainActivity.kt
+++ RecordKeeperApp/MainActivity.kt
@@ -6,11 +6,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.commit
 import com.example.recordkeeperapp.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
+    private lateinit var container : FrameLayout
+    private lateinit var bottomNav : BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +22,11 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        
+        this.container = binding.frameContent
+        this.bottomNav = binding.bottomNav
+
+        supportFragmentManager.commit {
+            add(R.id.frame_content, RunningFragment())
+        }
     }
 }
